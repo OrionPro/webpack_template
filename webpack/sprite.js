df4a3963696e1a2c71e7e5fd05a9f4b3b49cfaf2
orionpro <orion79@rambler.ru>
2017-05-23 01:04:40 +0300

    1 10 23.05.2017

--- webpack/sprite.js
+++ webpack/sprite.js
@@ -2,28 +2,6 @@ const SpritesmithPlugin = require('webpack-spritesmith');
 const path = require('path');
 module.exports = function() {
 	return {
-		module: {
-			loaders: [
-				{
-					test: /\.sass$/,
-					loaders: [
-					'style-loader',
-					'css-loader',
-					'sass-loader'
-				]
-				},
-				{
-					test: /\.png$/,
-					loaders: [
-					'file-loader?name=i/[hash].[ext]'
-				]
-				}
-			]
-		},
-		resolve: {
-			//webpack 2:
-			modules: ["node_modules", "spritesmith-generated"]
-		},
 		plugins: [
 			new SpritesmithPlugin({
 				src: {
@@ -35,8 +13,32 @@ module.exports = function() {
 					css: path.resolve(__dirname, '../source/sass/sprite.sass')
 				},
 				apiOptions: {
-					cssImageRef: "sprite.png",
-					padding: 5
+					cssImageRef: "sprite.png"
+				},
+				spritesmithOptions: {
+					padding: 2
+				},
+				spritesmithConfig: {
+					algorithm: 'binary-tree'
+				}
+			}),
+			new SpritesmithPlugin({
+				src: {
+					cwd: path.resolve(__dirname, '../source/img/sprite'),
+					glob: '*.png'
+				},
+				target: {
+					image: path.resolve(__dirname, '../source/pages/blog/sprite.png'),
+					css: path.resolve(__dirname, '../source/sass/sprite.sass')
+				},
+				apiOptions: {
+					cssImageRef: "sprite.png"
+				},
+				spritesmithOptions: {
+					padding: 2
+				},
+				spritesmithConfig: {
+					algorithm: 'binary-tree'
 				}
 			})
 		]
