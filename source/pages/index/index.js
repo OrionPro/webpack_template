afe392ac3f602d8e1945c187beb2e7fe22e1e3da
orion <orion79@rambler.ru>
2017-06-09 15:08:01 +0300

    15 50 9.06.2017

--- source/pages/index/index.js
+++ source/pages/index/index.js
@@ -1,7 +1,6 @@
 
 import '../../sass/main.sass';
 import './index.sass';
-import '../../sass/_media.sass';
 import '../../pages/index/index.pug'; //это для обновления страницы при hotreload - при npm build убрать
 
 require("../../libs/libs").jqueryui();
