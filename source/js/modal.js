0983db21c6dbbe1a68629f92938bb12d3dce052c
OrionPro <orion79@rambler.ru>
2017-10-10 14:54:45 +0300

    15 00 10.10.2017

--- source/js/modal.js
+++ source/js/modal.js
@@ -26,9 +26,14 @@ $(document).ready(function () {
 // 	// Открываем модальное окно
 	$(".open-popup-link").click(function () {
 		let id = $(this).attr('href');
+		let idBtn = $(this).data('mfp-src');
 		let txt = $(this).data('info');
 		// var title =  $(this).data('title'); // для изменения title в модалке
-		$(`.popup${id} input[name=form_name]`).val(txt);
+		if(id) {
+			$(`.popup${id} input[name=form_name]`).val(txt);
+		} else if(idBtn) {
+			$(`.popup${idBtn} input[name=form_name]`).val(txt);
+		}
 		// $(`.popup${id} .modal-title`).html(title); // прописать в ссылку data-title="нужный title"
 		if (window.matchMedia("(min-width: 992px)").matches) {
 			if (get_name_browser() == "Google Chrome") {
