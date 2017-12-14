65e738f62cb58af56a453bb2ce4214f8e242c587
OrionPro <orion79@rambler.ru>
2017-12-14 17:38:51 +0200

    17 45 14.12.2017

--- source/pages/index/index.js
+++ source/pages/index/index.js
@@ -46,12 +46,12 @@ $(document).ready(function () {
 	$(".select2").select2({
 		//minimumResultsForSearch: -1, // выключам поле ввода поиска
 		tags: false,
-		width: null
+		width: '100%'
 	});
 	$(".select2-tags").select2({
 		tags: true,
 		placeholder: "Выберите один или несколько тегов",
-		width: null // если null то будет шириной родителя
+		width: '100%'
 	});
 	// Инициализация маски в input
 	$(".mask").mask("+38(999) 999-99-99");
